9c17201c1218a981a4d6d7a4a5ba3b30ba2ec179
ArslanMD Official <numberkliye@gmail.com>
2025-11-04 14:34:48 +0500

    𝐀𝐑𝐒𝐋𝐀𝐍-𝐓𝐄𝐂𝐇

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "QADEER-AI~nJ8EALCB#44CVlZz0PP9TsZUJCk-h3niAj_tisAV5W7fBLtfvTCc",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
